c7a0b07fec4093c704568e6db8759dd8e1eb2956
Mark Keenan <marcokeeman@gmail.com>
2023-04-07 23:01:00 +0900

    Complete basic UI
    HTML and JS have been rewritten to add a UI to the game.

--- rps1.js
+++ rps1.js
@@ -1,3 +1,13 @@
+const results = document.querySelector('#results');
+const score = document.querySelector('#score');
+const finalResult = document.querySelector('#finalResult');
+const buttons = document.querySelectorAll('.button');
+buttons.forEach((button)=> {
+    button.addEventListener('click', (e) => {
+        let choice = e.target.id;
+        playRPS(choice);
+    });
+});
 //randomly choose PC play
 function getComputerChoice(){
     let comp = Math.random();
@@ -12,40 +22,42 @@ function getComputerChoice(){
     return compThrow;
 }
 
+let wins = 0;
+let losses = 0;
+let ties = 0;
 //take player choice input and compare player choice to PC to determine winner
-function playRPS(playerSelection = prompt("Chose your throw"), computerSelection = getComputerChoice()){
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    if ((playerSelection=="Rock"&&computerSelection == "Paper") || (playerSelection == "Paper"&& computerSelection == "Scissors") || (playerSelection == "Scissors"&&computerSelection == "Rock")){
-        return "You Lose! "+computerSelection+ " beats "+playerSelection+".";
-    } else if ((playerSelection == "Rock"&&computerSelection == "Scissors") || (playerSelection == "Paper"&& computerSelection == "Rock") || (playerSelection == "Scissors"&&computerSelection == "Paper")){
-        return "You Win! "+playerSelection+ " beats "+computerSelection+".";
-    } else {
-        return "It's a tie!";
-    } 
-}
 
-//play 5 rounds, display result
-function game(){
-    let wins = 0;
-    let losses = 0;
-    let ties = 0;
-    for (let i=0; i<5; i++){
-        let result = playRPS();
-        if (result.slice(0,6)=="You Wi"){
-            wins++;
-        } else if (result.slice(0,6)=="You Lo"){
+function playRPS(playerSelection, computerSelection = getComputerChoice()){
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (wins == 5 || losses == 5){
+        wins = 0;
+        losses = 0;
+        ties = 0;
+        results.textContent="";
+        showScore();
+        finalResult.textContent = "";
+    }
+        if ((playerSelection=="Rock"&&computerSelection == "Paper") || (playerSelection == "Paper"&& computerSelection == "Scissors") || (playerSelection == "Scissors"&&computerSelection == "Rock")){
+            results.textContent = "You Lose! "+computerSelection+ " beats "+playerSelection+".";
             losses++;
+            showScore();
+            if(losses == 5){
+                finalResult.textContent = "You Lose!"}
+                return;
+        } else if ((playerSelection == "Rock"&&computerSelection == "Scissors") || (playerSelection == "Paper"&& computerSelection == "Rock") || (playerSelection == "Scissors"&&computerSelection == "Paper")){
+            results.textContent = "You Win! "+playerSelection+ " beats "+computerSelection+".";
+            wins++;
+            showScore();
+            if(wins==5){
+                finalResult.textContent = "You Win!";}
+                return;
         } else {
+            results.textContent = "It's a tie!";
             ties++;
-        }
-        console.log(result);
+            showScore();
+        } 
     }
-    console.log("Wins: "+wins+", Losses: "+losses+", Ties: "+ties);
-    if(wins > losses){
-        console.log("You win!");
-    }else if (losses > wins){
-        console.log("You lose!");
-    }else {
-        console.log("It's a tie!");
-    }
-}
+    
+
+function showScore(){score.textContent = "Wins: "+wins+ " Losses: "+
+  losses+ " Ties: "+ties;}
